21fa71adb8455654ddd9fa5ee1e75beb224f1eb5
paulaolmedo <paulabeatrizolmedo@gmail.com>
2021-03-04 13:39:21 -0300

    update daemon to detect when the copied file is ready to be read/written, and to calculate the ndvi

--- src/service/image_detector.py
+++ src/service/image_detector.py
@@ -10,64 +10,36 @@ from inotify_simple import INotify, flags
 
 
 class EventHandler(FileSystemEventHandler):
-    def on_created(self, event): #detecta cada una de las carpetas interiores ??????
-        #regex = r"(MSIL1C_)(\d{8})(\/)(IMG_DATA)$"  # detecta la carpeta una vez que pasa por el proceso de renombrado. es exclusivo de los de sentinel leider     
+    def on_created(self, event): 
         regex = r"(.tif)|(.jp2)"  # TODO parametrizar
         matches = re.finditer(regex, event.src_path)
         images_path = ''
 
         inotify = INotify()
 
-
         for match in matches:
             images_path = os.path.dirname(event.src_path) + '/'
             img = os.path.basename(event.src_path)
-            timout_s = 10
-            timed_out = False
-            start_ts = time.monotonic()
-                
-            wd = inotify.add_watch(images_path, watch_flags)
+            
             watch_flags = flags.CLOSE_WRITE
-
-            print('toy acá')
-            inotify.read()
-            print("written " + img)
-
-        print('no hay matches todavía sorry bro')
-            
-            #while not timed_out:
-            #    try:
-            #        with open(img, 'w') as f:
-            #            #s = f.read()
-                        #print('read this bytes ' + len(s))
-            #            print("todo ok para " + img)
-            #            break
-            #    except IOError as e:
-            #            print('error ', e.errno, ',', e.strerror)
-
-            #    if (time.monotonic() - start_ts) > timout_s:
-            #        timed_out = True
+            wd = inotify.add_watch(images_path, watch_flags)
         
-        #if os.path.isdir(images_path):
-            #red_exists = os.path.isfile(images_path + 'red_665_10.jp2')
-            #nir_exists = os.path.isfile(images_path + 'nir4_832_10.jp2')
-            #img 
-            #try:
-                #with open(images_path + 'red_665_10.jp2', 'rw') as f:
-                    #print("todo ok para " + images_path + 'red_665_10.jp2')
+            inotify.read() #bloquea el resto de los procesos hasta que se cierre la escritura del archivo que se está copiando
+            print('copied: ' + img)
 
-            #if red_exists and nir_exists:
-                #print("starting norm indexes calculation process...")
-                #ni = NormalizedDifferenceIndex(images_path, 'sentinel')
-                #ni.calculate_ndvi()
+        red_exists = os.path.isfile(images_path + 'red_665_10.jp2')
+        nir_exists = os.path.isfile(images_path + 'nir4_832_10.jp2')
 
-                #print("starting crop process...")
-                #support = Support()
-                #support.crop_process(images_path, 'ndvi.tif', data_path, '/places.json')
+        if red_exists and nir_exists:
+            print("starting ndvi calculation process...")
+            ni = NormalizedDifferenceIndex(images_path, 'sentinel')
+            ni.write_ndvi_image()
+
+            print("starting cropping process...")
+            support = Support()
+            support.crop_process(images_path, 'ndvi.tif', images_path, '/places.json')
 
         
-            
-
 def image_daemon(server_path):
     observer = Observer()
     event_handler = EventHandler()
@@ -83,4 +55,10 @@ def image_daemon(server_path):
 
 
 # start the daemon process
-image_daemon('file_folder')
+server_path = 'test'
+if os.path.isdir(server_path) == True:
+    print('starting daemon')
+    image_daemon(server_path)
+else:
+    print('server path not found')
+
