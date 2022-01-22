7a998d717056a1135bfc508a05a4c8032c68a8e8
paulalmedo <paulabeatrizolmedo@gmail.com>
2022-01-22 19:31:29 -0300

    add some improvements: add functions annotations add call to calculate_specifics_mask

--- src/safo_impro/service/image_processor.py
+++ src/safo_impro/service/image_processor.py
@@ -10,6 +10,12 @@ class ImageProcessor:
         self.MRD = ManageRasterData()
 
     def normalized_indexes_calculation(self):
+        """
+        @WIP
+        calculates the corresponding normalized indexes, and reprojects them
+        parameters to add: type of index (++), filename, coordinate system (-)
+        """
+
         self.NI.write_new_raster('ndvi')  # parametrizar
 
         input = self.image_path + 'ndvi.tif'  # parametrizar
@@ -17,10 +23,23 @@ class ImageProcessor:
 
         self.MRD.reprojection('EPSG:4326', input, output)
 
+        masked = self.image_path + 'ndvi-masked.tif'
+        self.MRD.calculate_specifics_mask(output, masked)
+
         os.remove(input)
         return output
 
     def statistics_calculation(self):
+        """
+            este es el primer paso
+            primero calcula el ndvi en general
+            y luego le saca las estadísticas
+
+            @WIP
+            habría que agregar un paso al medio para que enmascare lo que no tiene que calcular
+            eso por un lado sería mas fácil rápido? para hacer los calculos
+            pero por el otro lado sería más fácil agarrarlo al final una vez que está calculado el ndvi porque es una sola capa...
+        """
         self.normalized_indexes_calculation()  # que haga el calculo que tiene que hacer
         output = self.MRD.statistics_process(self.image_path, self.image_path)
 
