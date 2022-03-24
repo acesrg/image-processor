17af27e07e5e6a623591fb0874bb0715ca034aba
paulalmedo <paulabeatrizolmedo@gmail.com>
2022-03-24 19:30:44 -0300

    add path-check

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+from os.path import exists
 from safo_impro.service.image_processor import ImageProcessor
 
 parser = argparse.ArgumentParser(description="Image processor")
@@ -39,6 +40,16 @@ level_dict = {
     'DEBUG': logging.DEBUG,
     'INFO': logging.INFO,
 }
+
+shp_path = args.shp_path
+image_path = args.image_path
+
+if not exists(shp_path):
+    print("No such file: " + shp_path)
+    quit()
+elif not exists(image_path):
+    print("No such file: " + image_path)
+    quit()
 
 IP = ImageProcessor(args.shp_path, args.image_path, args.operation, level_dict[level])
 IP.statistics_calculation()
